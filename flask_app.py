342afc89a98598c72ecd1e2b0f4c104fb76c1f03
Henrique Miranda <henrique@disroot.org>
2019-06-12 07:11:23 -0300

    Fix last user bug

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 
+
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
@@ -8,14 +9,11 @@ import os
 app = Flask(__name__)
 
 users = {'Henrique': [1, 5, 9], 'Jony': [2, 6, 10], 'Thiago': [3, 7, 11], 'Samuel': [4, 8, 12]}
-lastuser = 'Thiago'
-lastdate = '12/3/2019'
 
 @app.route('/')
 def hello_world():
     data = datetime.datetime.now().date()
     now = f'{data.day}/{data.month}/{data.year}'
-    d = lastdate.split('/')
 
     currentuser = ''
     for u in users.keys():
@@ -27,8 +25,10 @@ def hello_world():
         nextuser = usr[usr.index(currentuser) + 1]
     except:
         nextuser = usr[0]
+        
+    lastuser = usr[usr.index(currentuser) - 1]
 
-    dados = {'currentuser': currentuser, 'lastuser': lastuser, 'lastdate': lastdate, 'nextuser': nextuser, 'now': now}
+    dados = {'currentuser': currentuser, 'lastuser': lastuser, 'nextuser': nextuser, 'now': now}
 
     return render_template("home.html", dados=dados)
 
